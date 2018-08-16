b39b40397290a423742b386fc8020cdbb5ff876b
niexinming <993025447@qq.com>
2018-08-16 11:14:58 +0800

    debug False

--- start_test_hook.sh
+++ start_test_hook.sh
@@ -11,11 +11,14 @@ fi
 
 sleep 2
 
-python test_flask.py &
+python2 test_flask.py &
 
 sleep 1
 
 mypid=`ps -ax | grep 'test_flask.py' | grep -v 'grep' | awk '{print $1}'`
 
+echo $mypid
+
 sudo auditctl -a exclude,always -F msgtype!=PATH -F msgtype!=SYSCALL
+sudo auditctl -a always,exit -F arch=b64 -S execve -k rule01_exec_command
 sudo auditctl -a always,exit -F pid=$mypid
--- test_flask.py
+++ test_flask.py
@@ -92,4 +92,4 @@ def mysql():
 
 if __name__ == '__main__':
     
-    app.run(use_debugger=True, debug=True,host='0.0.0.0')
+    app.run(use_debugger=True, debug=False,host='0.0.0.0')
